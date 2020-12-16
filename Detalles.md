2c5e4dfd32bba0162a94c210053e900f9c21e21f
TBmex <cafeinauv@gmail.com>
2020-12-16 16:33:16 +0100

    Veamos el fastbaps

---
+++ Detalles.md
@@ -0,0 +1,11 @@
+## Nuevos planteos
+
+1. Jugar con el arbol retirando la raiz
+
+2. Dejar solo una muestra por cluster
+
+3. Revisar si el baps esta agrupando los clusters
+- El baps esta agrupando clusters de manera correcta, solo cuando se baja la profundidad demasiado cambia.
+
+4. Realizar con FastBaps
+- Confundido en este punto.
---
+++ vamos_lvl6/baps_1177_02.R
@@ -0,0 +1,31 @@
+#Run hierBAPS.
+#install.packages('rhierbaps')
+library(rhierbaps)
+library(ggtree)
+library(phytools)
+library(ape)
+set.seed(1234)
+
+#Plotting results
+newick.file.name <- "run_alignment_no_resis_1177.fas.treefile"
+iqtree_1177 <- phytools::read.newick(newick.file.name)
+
+baps_1177 <- read.csv("hierbaps_partition_1177_l4.csv", sep = ",", header = T)
+baps_1177$Isolate <- as.character(baps_1177$Isolate)
+baps_1177$level.1 <- as.double(baps_1177$level.1)
+baps_1177$level.2 <- as.double(baps_1177$level.2)
+
+#A simple coloured tree allows us to see the top level cluster assignment from hierBAPS.
+gg <- ggtree(iqtree_1177, layout = "circular")
+gg_1177 <- gg %<+% baps_1177
+gg_1177 <- gg_1177 + geom_tippoint(aes(color = factor(level.1)))
+gg_1177
+
+gg <- ggtree(iqtree_1177, layout = "circular", branch.length = "none")
+gg_1177 <- gg %<+% baps_1177
+gg_1177 <- gg_1177 + geom_tippoint(aes(color = factor(level.3)))
+gg_1177 <- gg_1177 + theme(legend.position = "right")
+gg_1177 <- gg_1177 + geom_tiplab(aes(label = level.3), size = 1, offset = 1)
+gg_1177
+
+plot_sub_cluster(hb.results, iqtree_1177, level = 3, sub.cluster = 56)
